67b32071d5500bb2efa19704ce51a561b1d06855
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-15 10:10:39 -0700

    saving response to appstate

--- app/services/UnsandboxImagesService.js
+++ app/services/UnsandboxImagesService.js
@@ -1,9 +1,14 @@
+import { AppState } from "../AppState.js";
+import { UnsandboxImage } from "../models/UnsandboxImage.js";
 import { unsandboxAPI } from "./AxiosService.js"
 
 class UnsandboxImagesService {
   async getRandomImage() {
     const response = await unsandboxAPI.get('api/images/random')
     console.log('📡 got random image', response.data);
+    const newImage = new UnsandboxImage(response.data)
+    console.log('new image', newImage);
+    AppState.unsandboxImage = newImage
   }
 }
 
