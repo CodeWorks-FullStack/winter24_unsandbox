81b24ca693e5fab63fe122cbd5ffe4ea10ecc757
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-15 10:17:00 -0700

    drawing random image

--- app/controllers/UnsandboxImagesController.js
+++ app/controllers/UnsandboxImagesController.js
@@ -1,10 +1,18 @@
+import { AppState } from "../AppState.js";
 import { unsandboxImagesService } from "../services/UnsandboxImagesService.js";
 import { Pop } from "../utils/Pop.js";
+
+function _drawUnsandboxImage() {
+  const unsandboxImage = AppState.unsandboxImage
+  document.body.style.backgroundImage = `url(${unsandboxImage.imgUrl})`
+}
 
 export class UnsandboxImagesController {
   constructor () {
     console.log('Unsandbox controller loaded');
     this.getRandomImage()
+
+    AppState.on('unsandboxImage', _drawUnsandboxImage)
   }
 
   async getRandomImage() {
